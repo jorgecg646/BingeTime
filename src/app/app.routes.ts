ef4e8658e25b9b16b6ed087616c6352dbc64247d
DESKTOP-LFS7KLR\Jorge Castillo <jorgecg646@gmail.com>
2026-08-21 10:08:03 +0200

    Fix: Landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { TopSeriesComponent } from './components/top-series/top-series.component';
 import { YourShowsPageComponent } from './components/your-shows-page/your-shows-page.component';
 import { AuthComponent } from './components/auth/auth.component';
-import { authGuard, guestGuard } from './guards/auth.guard';
+import { guestGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
   { path: 'auth', component: AuthComponent, canActivate: [guestGuard] },
-  { path: '', component: YourShowsPageComponent, canActivate: [authGuard] },
+  { path: '', component: YourShowsPageComponent },
   { path: 'discover', component: HomeComponent },
   { path: 'top-series', component: TopSeriesComponent },
   { path: '**', redirectTo: '' }
